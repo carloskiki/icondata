78cf92ae52bdf901edc7a341eb49613899863a16
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2023-05-01 18:35:00 -0400

    icondata_core 0.0.2 for everything

--- icon_index/src/lib.rs
+++ icon_index/src/lib.rs
@@ -1,6 +1,25 @@
+
 use leptos_icons::*;
 use strum::IntoEnumIterator;
 
-fn all_icons() -> impl Iterator<Item = icondata_core::IconData> {
-    let a: icondata_core::IconData = AiIcon::AiApiFilled.into();
+fn all_icons() -> impl Iterator<Item = IconData> {
+    [
+        AiIcon::iter(),
+        FaIcon::iter(),
+        WiIcon::iter(),
+        FiIcon::iter(),
+        VsIcon::iter(),
+        BsIcon::iter(),
+        BiIcon::iter(),
+        ImIcon::iter(),
+        IoIcon::iter(),
+        RiIcon::iter(),
+        SiIcon::iter(),
+        TiIcon::iter(),
+        HiIcon::iter(),
+        CgIcon::iter(),
+        TbIcon::iter(),
+        OcIcon::iter(),
+        LuIcon::iter(),
+    ]
 }
