47f26e63b23fad0254512491d25f1d648ba871c3
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2023-05-19 12:54:28 -0400

    added general Icon enum

--- build/templates/main_lib/lib.rs
+++ build/templates/main_lib/lib.rs
@@ -7,11 +7,31 @@
 //! identical.
 //!
 pub use icondata_core::IconData;
-{% for lib_short in lib_names %}
-#[cfg!(feature = "{{lib_short|capitalize}}")]
+{% for short_name in short_names %}
+#[cfg!(feature = "{{short_name|capitalize}}")]
 pub use icondata_{{short_name}}::*;
 {% endfor %}
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum Icon {
 
 }
+impl<'a> icondata_core::IconData<'a> for crate::Icon {
+    fn data(self) -> &'a icondata_core::Data {
+        match self {
+            {% for short_name in short_names %}
+            #[cfg(feature = "{{short_name|capitalize}}")]
+            Self::{{short_name|capitalize}}(icon) => icondata_core::IconData::from(icon),
+            {% endfor %}
+        }
+    }
+}
+{% for short_name in short_names %}
+#[cfg(feature = "{{short_name|capitalize}}")]
+impl From<{{short_name|capitalize}}Icon> for Icon {
+    fn from(value: {{short_name|capitalize}}Icon) -> Self {
+        Self::{{short_name|capitalize}}(value)
+    }
+}
+{% endfor %}
