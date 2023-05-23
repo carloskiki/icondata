ffe5cde7d997d56a72651251a72860960bad6bd5
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2023-05-22 20:18:58 -0400

    better docs

--- build/templates/main_lib/lib.rs
+++ build/templates/main_lib/lib.rs
@@ -1,10 +1,12 @@
 //! This crate provides a collection of icons in the form of SVG data
-//! and an enum to select them.
+//! and an enum to select them. It re-exports all icons from the icondata_* crates.
 //!
-//! ## Usage
+//! This crate is meant to be used to build component libraries for web frameworks.
+//! To do so, an [`Icon`] enum is provided, which can be used to select any icon from the
+//! collection. This enum is marked as non_exhaustive, as new icon libraries may be added in the future.
 //!
-//! Every icon is shipped as its own feature; the enum variant and their corresponding feature name are
-//! identical.
+//! The [`Icon`] enum can be converted into an [`IconData`] struct, which contains the SVG data.
+//!
 //!
 pub use icondata_core::IconData;
 {% for short_name in short_names %}
@@ -12,6 +14,12 @@ pub use icondata_core::IconData;
 pub use icondata_{{short_name}}::*;
 {%- endfor %}
 
+/// The main enum to select an icon. This enum contains all icons from icondata_* libraries, and
+/// implements [`From`] for [`IconData`], so it can be converted into an [`IconData`] struct.
+/// It also implements [`From`] for any icon in any icondata_* library.
+///
+/// This enum is marked as non_exhaustive, as new icon libraries may be added in the future.
+#[non_exhaustive]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum Icon {
